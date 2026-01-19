0bab6e28eda80452df50df7a481c1b06dc6e8edc
levelstage <chsu2040@yonsei.ac.kr>
2026-01-19 16:40:11 +0900

    refactor: Adam 밑바닥부터 구현

--- Networks/optimizer.py
+++ Networks/optimizer.py
@@ -9,20 +9,28 @@ class Adam:
         self.m = None
         self.v = None
         
-    def update(self, params, grads):
+    def update(self, net, grad):
         if self.m is None:
-            self.m, self.v = {}, {}
-            for key, val in params.items():
+            self.m = {}
+            self.v = {}
+            for key, val in grad.items():
                 self.m[key] = np.zeros_like(val)
                 self.v[key] = np.zeros_like(val)
-        
+        # 초기값이 0이기 때문에 정상적인 학습을 위해 값을 보정해줘야함.
         self.iter += 1
-        lr_t  = self.lr * np.sqrt(1.0 - self.beta2**self.iter) / (1.0 - self.beta1**self.iter)
-        
-        for key in params.keys():
-            # 모멘텀과 RMSProp 계산
-            self.m[key] += (1 - self.beta1) * (grads[key] - self.m[key])
-            self.v[key] += (1 - self.beta2) * (grads[key]**2 - self.v[key])
-            
-            # 파라미터 업데이트 (여기가 핵심!)
-            params[key] -= lr_t * self.m[key] / (np.sqrt(self.v[key]) + 1e-7)
+        j=1
+        for layer in net.layers.values():
+            if hasattr(layer, 'W'):
+                # 가중치 갱신
+                self.m['W'+str(j)] = self.beta1 * self.m['W'+str(j)] + (1-self.beta1) * grad['W'+str(j)]
+                self.v['W'+str(j)] = self.beta2 * self.v['W'+str(j)] + (1-self.beta2) * (grad['W'+str(j)]**2)
+                m_corrected = self.m['W'+str(j)] / (1 - self.beta1 ** self.iter)
+                v_corrected = self.v['W'+str(j)] / (1 - self.beta2 ** self.iter)
+                layer.W -= self.lr * m_corrected / (np.sqrt(v_corrected) + 1e-8)
+                # 바이어스 갱신
+                self.m['b'+str(j)] = self.beta1 * self.m['b'+str(j)] + (1-self.beta1) * grad['b'+str(j)]
+                self.v['b'+str(j)] = self.beta2 * self.v['b'+str(j)] + (1-self.beta2) * (grad['b'+str(j)]**2)
+                m_corrected = self.m['b'+str(j)] / (1 - self.beta1 ** self.iter)
+                v_corrected = self.v['b'+str(j)] / (1 - self.beta2 ** self.iter)
+                layer.b -= self.lr * m_corrected / (np.sqrt(v_corrected) + 1e-8)
+                j+=1
