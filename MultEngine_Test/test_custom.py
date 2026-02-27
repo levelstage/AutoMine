59dcea34e2bfebdf8bda2d83dffc5599c739f0c4
levelstage <chsu2040@yonsei.ac.kr>
2026-02-27 15:28:05 +0900

    refactor: AI가 만든 군더더기 코드 삭제

--- MultEngine_Test/test_custom.py
+++ MultEngine_Test/test_custom.py
@@ -3,34 +3,6 @@ import sys
 import numpy as np
 import time
 
-# --- [Windows DLL  등록] ---
-# MKL이 몰래 끌어다 쓰는 TBB, OpenMP 등의 경로를 모조리 등록합니다.
-oneapi_root = r"C:\Program Files (x86)\Intel\oneAPI"
-print("의존성 DLL 경로 등록 중...")
-
-# 현재 스크립트 위치 기준으로 build 폴더를 sys.path에 추가 (모듈 검색용)
-sys.path.append(os.path.join(os.path.dirname(__file__), 'build'))
-sys.path.append(os.path.dirname(__file__))
-
-# 인텔 주요 라이브러리 경로들
-extra_dll_paths = [
-    r"compiler\latest\bin",
-    r"mkl\latest\bin",
-    r"tbb\latest\bin",       # <-- 이게 없어서 터졌을 확률이 매우 높습니다 (스레딩 빌딩 블록)
-    r"compiler\latest\windows\redist\intel64_win\compiler", # OpenMP (libiomp5md.dll)
-    r"mkl\latest\redist\intel64",
-    r"compiler\latest\windows\bin"
-]
-
-for sub in extra_dll_paths:
-    p = os.path.join(oneapi_root, sub)
-    if os.path.exists(p):
-        try:
-            os.add_dll_directory(p)
-            print(f"  -> 등록 완료: {p}")
-        except Exception:
-            pass
-print("-" * 50)
 import sycl_mat
 
 size = 4096
