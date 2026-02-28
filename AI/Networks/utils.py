52e64c7de688fae15bb8ed26e7ee26b3d2018d13
levelstage <chsu2040@yonsei.ac.kr>
2026-02-28 15:45:50 +0900

    refactor: im2col 함수 개선(numpy의 as_strided 활용)

--- AI/Networks/utils.py
+++ AI/Networks/utils.py
@@ -1,18 +1,26 @@
 import numpy as np;
 
 # im2col 함수
-
 def im2col(im, filter_h, filter_w, stride, pad):
+    # N, C, H, W
     N, C, H, W = im.shape
-    OH = (H+2*pad-filter_h)//stride + 1
-    OW = (W+2*pad-filter_w)//stride + 1
-    col = np.zeros((N*OH*OW, C*filter_h*filter_w))
-    padded = np.pad(im, ((0,0), (0,0), (pad, pad), (pad, pad)))
-    for i in range(N):
-        for y in range(OH):
-            for x in range(OW):
-                col[i * OH * OW + y * OW + x,:] = padded[i, :, y*stride:y*stride+filter_h, x*stride:x*stride+filter_w].reshape((1, C*filter_h*filter_w))
-    return col
+    out_h = (H + 2*pad - filter_h) // stride + 1
+    out_w = (W + 2*pad - filter_w) // stride + 1
+
+    # 패딩 처리
+    img = np.pad(im, [(0,0), (0,0), (pad, pad), (pad, pad)], 'constant')
+    
+    # [N, C, H, W] 이미지에서 [N, C, out_h, out_w, filter_h, filter_w] 6차원 뷰를 생성
+    # 메모리 복사 없이 stride(보폭)만 조작해서 겹치는 부분을 표현함 (매우 빠름)
+    col = np.lib.stride_tricks.as_strided(
+        img,
+        shape=(N, C, out_h, out_w, filter_h, filter_w),
+        strides=(img.strides[0], img.strides[1], img.strides[2]*stride, img.strides[3]*stride, img.strides[2], img.strides[3])
+    )
+
+    # 행렬 곱셈을 위해 (N*OH*OW, C*FH*FW) 형상으로 정렬 후 반환
+    res = col.transpose(0, 2, 3, 1, 4, 5).reshape(N*out_h*out_w, -1)
+    return np.ascontiguousarray(res, dtype=np.float32)
 
 #  col2im 함수
 
