aef7e532614fbe05d2ea7e9243b5ff844918beb6
levelstage <chsu2040@yonsei.ac.kr>
2026-03-15 20:06:14 +0900

    refactor: layers.py를 numpy/mkl 버전으로 분리
    - layers.py -> layers_mkl.py rename (MKL 기반)
    - layers.py 새로 생성 (NumPy + utils.im2col/col2im 기반)
    - layers_mkl.py 타이밍 코드 및 주석 정리

--- AI/Networks/layers_mkl.py
+++ AI/Networks/layers_mkl.py
@@ -1,6 +1,6 @@
 import numpy as np
+import mkl_mat as mkl
 import math
-from . import utils
 
 class ReLU:
     def __init__(self):
@@ -64,9 +64,9 @@ class Conv2d:
         out_w = (W + 2*self.pad - FW) // self.stride + 1
 
         # im2col로 입력을 2D 행렬로 펼쳐서 합성곱을 행렬곱으로 처리한다
-        col = utils.im2col(x, FH, FW, self.stride, self.pad)
+        col = mkl.im2col(x, FH, FW, self.stride, self.pad).astype(np.float32)
         col_W = self.W.reshape(FN, -1)
-        out = col @ col_W.T + self.b
+        out = mkl.matmul(col, col_W, a_T=False, b_T=True) + self.b
         out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
 
         # 역전파에서 dW, dx 계산에 필요하므로 저장해둔다
@@ -79,16 +79,22 @@ class Conv2d:
         N, C, H, W = self.x.shape
 
         # 행렬곱 형태로 맞추기 위해 dout을 (N*OH*OW, FN) 형상으로 변환한다
-        dout_reshaped = dout.transpose(0, 2, 3, 1).reshape(-1, FN)
+        dout_reshaped = np.ascontiguousarray(dout.transpose(0, 2, 3, 1).reshape(-1, FN), dtype=np.float32)
         self.db = np.sum(dout_reshaped, axis=0)
 
         # col.T @ dout으로 dW를 구한 뒤 필터 형상으로 복원한다
-        self.dW = (self.col.T @ dout_reshaped).T.reshape(FN, C, FH, FW)
+        self.dW = mkl.matmul(self.col, dout_reshaped, a_T=True, b_T=False).T.reshape(FN, C, FH, FW)
 
         # dout @ col_W로 dx_col을 구한 뒤 col2im으로 원래 형상으로 되돌린다
         col_W = self.W.reshape(FN, -1)
-        dx_col = dout_reshaped @ col_W
-        dx = utils.col2im(dx_col, (N, C, H, W), FH, FW, self.stride, self.pad)
+        dx_col = mkl.matmul(dout_reshaped, col_W, a_T=False, b_T=False)
+        dx_padded = mkl.col2im(dx_col, N, C, H, W, FH, FW, self.stride, self.pad)
+
+        # 순전파에서 추가했던 패딩을 제거한다
+        if self.pad > 0:
+            dx = dx_padded[:, :, self.pad:-self.pad, self.pad:-self.pad]
+        else:
+            dx = dx_padded
         return dx
 
 class Affine:
@@ -112,15 +118,16 @@ class Affine:
         # 역전파에서 dW 계산에 필요하므로 입력을 저장해둔다
         self.x = x
         x_reshaped = x.reshape(-1, self.input_size).astype(np.float32)
-        out = x_reshaped @ self.W + self.b
+        out = mkl.matmul(x_reshaped, self.W) + self.b
         return out
 
     def backward(self, dout):
-        self.db = np.sum(dout, axis=0)
+        dout_contig = np.ascontiguousarray(dout, dtype=np.float32)
+        self.db = np.sum(dout_contig, axis=0)
 
         x_reshaped = self.x.reshape(-1, self.input_size).astype(np.float32)
-        self.dW = x_reshaped.T @ dout      # x.T @ dout
-        dx = (dout @ self.W.T).reshape(self.x.shape)  # dout @ W.T
+        self.dW = mkl.matmul(x_reshaped, dout_contig, a_T=True)   # x.T @ dout
+        dx = mkl.matmul(dout_contig, self.W, b_T=True).reshape(self.x.shape)  # dout @ W.T
         return dx
 
 class BinaryCrossEntropy:
